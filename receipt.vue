70ecc772d655b6553c1a640da7093bc505d0c868
dani <killoooonnnn@hotmail.com>
2019-10-16 12:20:25 +0200

    Started migration to vue.js

---
+++ receipt.vue
@@ -0,0 +1,20 @@
+Vue.component('receipt-item', {
+  props: ['receipt'],
+  template: '
+  	<li class="receipt {{v-if=receipt.is_new}}receipt--new" data-category={{receipt.category}} draggable="true">
+		<div class="receipt_name">
+			{{ receipt.name }}
+			<span v-if="receipt.ingredients" class="ingredients_trigger">-i-</span>
+			<span v-if="receipt.link"><a href={{ receipt.link }} target='_blank'>+</a></span>
+			<span v-if="receipt.is_new" class="label label-success">New!</span>
+		</div>
+		<div class='receipt_image img-wrapper'>
+			<img class='img-responsive' src='assets/img/{{ receipt.image }}.jpg'
+				onerror="this.onerror=null;this.src='assets/img/placeholder.jpg';">
+		</div>
+		<div v-if="receipt.ingredients" class="receipt_ingredients hide">
+			{{ receipt.ingredients }}
+		</div>
+	</li>
+  	'>
+})
